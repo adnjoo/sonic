9e3f20e7519f05ad016c0df2a8ba45714cb4fb04
Andrew Njoo <adnjoo@gmail.com>
2021-12-09 13:56:58 -0800

    edit

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,11 @@
 import { getProviders, signIn } from "next-auth/react";
 
 function Login({ providers }) {
+  console.log('providers',providers)
   return (
     <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
       <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="" />
+
 
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
