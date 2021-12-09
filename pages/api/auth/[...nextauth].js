22655a5421992a0a2e13a0e5f992d911123bb1e8
Andrew Njoo <adnjoo@gmail.com>
2021-12-08 20:09:00 -0800

    edit nexauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,35 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 import { refreshAccessToken } from "spotify-web-api-node/src/server-methods";
+import spotifyApi, { LOGIN_ERROR } from "../../../lib/spotify";
+
+async function refreshAccessToken(token) {
+  try {
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setAccessToken(token.refreshToken);
+
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+    console.log("REFRESHED TOKEN IS", refreshedToken);
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      // 1 hour as 3600 returns from 
+      accessTokenExpires: Date.now + refreshedToken.expires_in * 1000,
+      // Spotify API
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+      // Replace if new one came back else fall back to old refresh token
+    };
+    
+  } catch (error) {
+    console.error(error);
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
