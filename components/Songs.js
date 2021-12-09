99ea8b60b640b91efad7023dacc5474ad669e148
Andrew Njoo <adnjoo@gmail.com>
2021-12-09 10:59:59 -0800

    edit

--- components/Songs.js
+++ components/Songs.js
@@ -1,26 +1,21 @@
-import {useRecoilValue} from 'recoil';
-import {playlistState} from '../atoms/playlistAtom';
+import { useRecoilValue } from "recoil";
+import { playlistState } from "../atoms/playlistAtom";
+import Song from "./Song";
 
 function Songs() {
-    const playlist = useRecoilValue(playlistState);
-    
-    function renderNames () {
-        if(playlist){
-            return playlist.tracks.items.map(x=>(
-                <div>
-                    {x.track.name}
-                </div>
-                ))
-        }
-        return (
-            'no playlist'
-        )
-    }
-    return (
-        <div className='text-white'>
-            {renderNames()}
+  const playlist = useRecoilValue(playlistState);
+
+  function renderNames() {
+    if (playlist) {
+      return playlist.tracks.items.map((track, i) => (
+        <div>
+          <Song key={track.track.id} track={track} order={i} />
         </div>
-    );
+      ));
+    }
+    return "no playlist";
+  }
+  return <div className="px-8 flex flex-col space-y-1 pb-28 text-white">{renderNames()}</div>;
 }
- 
-export default Songs
+
+export default Songs;
