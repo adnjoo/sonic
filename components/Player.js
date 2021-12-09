4a192bd6ae947f24d221b2ccb7379503f4cc4c99
Andrew Njoo <adnjoo@gmail.com>
2021-12-09 13:08:50 -0800

    edit

--- components/Player.js
+++ components/Player.js
@@ -1,7 +1,21 @@
-import {useEffect, useState} from 'react'
+import {
+  HeartIcon,
+  VolumeUpIcon as VolumeDownIcon,
+} from "@heroicons/react/outline";
+import {
+  FastForwardIcon,
+  PauseIcon,
+  PlayIcon,
+  ReplyIcon,
+  RewindIcon,
+  VolumeUpIcon,
+  SwitchHorizontalIcon,
+} from "@heroicons/react/solid";
+
+import { useEffect, useState } from "react";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import { useSession } from "next-auth/react";
-import {useRecoilState} from "recoil";
+import { useRecoilState } from "recoil";
 import useSpotify from "../hooks/useSpotify";
 import useSongInfo from "../hooks/useSongInfo";
 
@@ -16,42 +30,80 @@ function Player() {
   const songInfo = useSongInfo();
 
   const fetchCurrentSong = () => {
-      if (!songInfo) {
-          spotifyApi.getMyCurrentPlayingTrack().then(data => {
-            console.log('Now playing: ', data.body?.item);
-              setCurrentTrackId(data.body?.item?.id);
+    if (!songInfo) {
+      spotifyApi.getMyCurrentPlayingTrack().then((data) => {
+        console.log("Now playing: ", data.body?.item);
+        setCurrentTrackId(data.body?.item?.id);
 
-              spotifyApi.getMyCurrentPlaybackState().then(data=>{
-                  setIsPlaying(data.body?.is_playing);
-              })
-          })
-      }
-  }
+        spotifyApi.getMyCurrentPlaybackState().then((data) => {
+          setIsPlaying(data.body?.is_playing);
+        });
+      });
+    }
+  };
 
-  useEffect(()=>{
-      if(spotifyApi.getAccessToken() && !currentTrackId){
-          // fetch song info
-          fetchCurrentSong();
-          setVolume(50);
+  useEffect(() => {
+    if (spotifyApi.getAccessToken() && !currentTrackId) {
+      // fetch song info
+      fetchCurrentSong();
+      setVolume(50);
+    }
+  }, [currentTrackId, spotifyApi, session]);
+
+  const handlePlayPause = () => {
+    spotifyApi.getMyCurrentPlaybackState().then((data) => {
+      if (data.body.is_playing) {
+        spotifyApi.pause();
+        setIsPlaying(false);
+      } else {
+        spotifyApi.play();
+        setIsPlaying(true);
       }
-  },[currentTrackId,spotifyApi,session])
+    });
+  };
 
   return (
-    <div className='h-24 bg-gradient-to-b from-black-to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8'>
+    <div className="h-24 bg-gradient-to-b from-black-to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
       {/* Left */}
-      <div className='flex items-center space-x-4'>
+      <div className="flex items-center space-x-4">
         <img
-        className='hidden md:inline h-10 w-10'
-        src={songInfo?.album.images?.[0]?.url} alt="" />
+          className="hidden md:inline h-10 w-10"
+          src={songInfo?.album.images?.[0]?.url}
+          alt=""
+        />
         <div>
-            <h3>{songInfo?.name}</h3>
-            <p>{songInfo?.artists?.[0]?.name}</p>
+          <h3>{songInfo?.name}</h3>
+          <p>{songInfo?.artists?.[0]?.name}</p>
         </div>
       </div>
 
       {/* Center */}
-      <div>
-          
+      <div className="flex items-center justify-evenly">
+        <SwitchHorizontalIcon className="button" />
+        <RewindIcon
+          // onClick={() => spotifyApi.skipToPrevious()}
+          // the API is not working
+          className="button"
+        />
+
+        {isPlaying ? (
+          <PauseIcon onClick={handlePlayPause} className="button w-10 h-10" />
+        ) : (
+          <PlayIcon onClick={handlePlayPause} className="button w-10 h-10" />
+        )}
+
+        <FastForwardIcon
+          // onClick={() => spotifyApi.skipToNext()}
+          // the API is not working
+          className="button"
+        />
+        <ReplyIcon className="button" />
+      </div>
+      {/* Right */}
+      <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
+        <VolumeDownIcon className="button" />
+        <input className='w-14 md:w-28' type='range' value={volume} min={0} max={100} />
+        <VolumeUpIcon className="button" />
       </div>
     </div>
   );
