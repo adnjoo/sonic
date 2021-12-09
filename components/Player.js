fbcc19dd5c9a830ad0e7ceff2935aba92e3875a9
Andrew Njoo <adnjoo@gmail.com>
2021-12-09 13:21:44 -0800

    edit

--- components/Player.js
+++ components/Player.js
@@ -11,13 +11,13 @@ import {
   VolumeUpIcon,
   SwitchHorizontalIcon,
 } from "@heroicons/react/solid";
-
-import { useEffect, useState } from "react";
-import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
+import { debounce} from 'lodash';
 import { useSession } from "next-auth/react";
+import { useEffect, useState, useCallback } from "react";
 import { useRecoilState } from "recoil";
-import useSpotify from "../hooks/useSpotify";
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import useSongInfo from "../hooks/useSongInfo";
+import useSpotify from "../hooks/useSpotify";
 
 function Player() {
   const spotifyApi = useSpotify();
@@ -49,6 +49,17 @@ function Player() {
       setVolume(50);
     }
   }, [currentTrackId, spotifyApi, session]);
+
+  useEffect(()=>{
+      if(volume>0 && volume < 100){
+        debounceAdjustVolume(volume);
+      }
+  },[volume])
+
+  const debounceAdjustVolume = useCallback(
+    debounce((volume)=>{
+        spotifyApi.setVolume(volume).catch(err=>{});
+    },500),[])
 
   const handlePlayPause = () => {
     spotifyApi.getMyCurrentPlaybackState().then((data) => {
@@ -101,9 +112,22 @@ function Player() {
       </div>
       {/* Right */}
       <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
-        <VolumeDownIcon className="button" />
-        <input className='w-14 md:w-28' type='range' value={volume} min={0} max={100} />
-        <VolumeUpIcon className="button" />
+        <VolumeDownIcon
+          onClick={() => volume > 0 && setVolume(volume - 10)}
+          className="button"
+        />
+        <input
+          className="w-14 md:w-28"
+          type="range"
+          value={volume}
+          onChange={(e) => setVolume(Number(e.target.value))}
+          min={0}
+          max={100}
+        />
+        <VolumeUpIcon
+          onClick={() => volume < 100 && setVolume(volume + 10)}
+          className="button"
+        />
       </div>
     </div>
   );
