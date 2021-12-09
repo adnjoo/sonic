3c0e95025e04c329e69ab4ad8625257f12929848
Andrew Njoo <adnjoo@gmail.com>
2021-12-09 12:14:16 -0800

    add playSong function

--- components/Song.js
+++ components/Song.js
@@ -1,10 +1,26 @@
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import useSpotify from "../hooks/useSpotify";
 import { millisToMinutesAndSeconds } from "../lib/time";
+import {useRecoilState} from 'recoil'
 
 function Song({ order, track }) {
   const spotifyApi = useSpotify();
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+
+  const playSong = () => {
+      setCurrentTrackId(track.track.id)
+      setIsPlaying(true)
+      spotifyApi.play({
+          uris: [track.track.uri],
+      })
+  };
+
   return (
-    <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer">
+    <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer"
+    onClick={playSong}
+    >
       <div className="flex items-center space-x-4">
         <p>{order + 1}</p>
         <img
@@ -13,8 +29,8 @@ function Song({ order, track }) {
           alt=""
         />
         <div>
-          <p className='w-36 lg:w-64 text-white truncate'>{track.track.name}</p>
-          <p className='w-40'>{track.track.artists[0].name}</p>
+          <p className="w-36 lg:w-64 text-white truncate">{track.track.name}</p>
+          <p className="w-40">{track.track.artists[0].name}</p>
         </div>
       </div>
 
