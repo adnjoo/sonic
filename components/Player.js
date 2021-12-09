c951bf20ced1ffe5080aa6c9902f510096b8096d
Andrew Njoo <adnjoo@gmail.com>
2021-12-09 12:51:30 -0800

    create css file

--- components/Player.js
+++ components/Player.js
@@ -1,14 +1,57 @@
+import {useEffect, useState} from 'react'
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
+import { useSession } from "next-auth/react";
+import {useRecoilState} from "recoil";
 import useSpotify from "../hooks/useSpotify";
+import useSongInfo from "../hooks/useSongInfo";
 
 function Player() {
   const spotifyApi = useSpotify();
   const { data: session, status } = useSession();
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+  const [volume, setVolume] = useState(50);
+
+  const songInfo = useSongInfo();
+
+  const fetchCurrentSong = () => {
+      if (!songInfo) {
+          spotifyApi.getMyCurrentPlayingTrack().then(data => {
+            console.log('Now playing: ', data.body?.item);
+              setCurrentTrackId(data.body?.item?.id);
+
+              spotifyApi.getMyCurrentPlaybackState().then(data=>{
+                  setIsPlaying(data.body?.is_playing);
+              })
+          })
+      }
+  }
+
+  useEffect(()=>{
+      if(spotifyApi.getAccessToken() && !currentTrackId){
+          // fetch song info
+          fetchCurrentSong();
+          setVolume(50);
+      }
+  },[currentTrackId,spotifyApi,session])
 
   return (
-    <div>
+    <div className='h-24 bg-gradient-to-b from-black-to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8'>
       {/* Left */}
+      <div className='flex items-center space-x-4'>
+        <img
+        className='hidden md:inline h-10 w-10'
+        src={songInfo?.album.images?.[0]?.url} alt="" />
+        <div>
+            <h3>{songInfo?.name}</h3>
+            <p>{songInfo?.artists?.[0]?.name}</p>
+        </div>
+      </div>
+
+      {/* Center */}
       <div>
-        <img src="" alt="" />
+          
       </div>
     </div>
   );
