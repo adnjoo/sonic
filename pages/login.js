a529338862583f720b7cf6a9eefd1cbbbfc98aef
Andrew Njoo <adnjoo@gmail.com>
2023-05-19 18:20:37 -0700

    feat: add prettier tailwind

--- pages/login.js
+++ pages/login.js
@@ -1,23 +1,23 @@
-import { getProviders, signIn } from "next-auth/react";
+import { getProviders, signIn } from 'next-auth/react';
 
 function Login({ providers }) {
-  console.log('providers',providers)
+  console.log('providers', providers);
   return (
-    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
-      <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="" />
+    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-black">
+      <img className="mb-5 w-52" src="https://links.papareact.com/9xl" alt="" />
 
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
           <button
             onClick={() => {
-              signIn(provider.id, { callbackUrl: "/" })}}
-            className="bg-[#18d860] p-5 text-white p-5 rounded-full"
+              signIn(provider.id, { callbackUrl: '/' });
+            }}
+            className="rounded-full bg-[#18d860] p-5 text-white duration-300 hover:bg-[#109342]"
           >
             Login with: {provider.name}
           </button>
         </div>
-      ))} 
-
+      ))}
     </div>
   );
 }
@@ -29,7 +29,7 @@ export async function getServerSideProps() {
 
   return {
     props: {
-      providers,
-    },
+      providers
+    }
   };
 }
